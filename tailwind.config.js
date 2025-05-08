49db538a9e57e9b4ecd10336587ee1144153084c
Kieran911 <arinzekieran@gmail.com>
2025-05-08 08:48:12 +0100

    Added some files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.liquid', './**/*.json', './src/**/*.{html,js}'],
+  content: ['./**/*.liquid', './src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
