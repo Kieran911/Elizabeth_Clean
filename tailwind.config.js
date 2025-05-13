7b15767b0d981598b712842912c84b717a186c8e
Kieran911 <arinzekieran@gmail.com>
2025-05-13 15:06:54 +0100

    Add mobile swiper functionality to product template and update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './snippets/**/*.liquid',
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '94' : '22rem',
+      },
+    },
   },
   plugins: [],
 };
